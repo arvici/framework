69d5aa4df7f732511ec1e4b762fd50a8c311285b
Tom Valk <tomvalk@gmail.com>
2016-01-15 12:43:49 +0100

    Adding testing structure, and configs.

---
+++ tests/src/Arvici/Tests/Heart/Router/RouterTest.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Router Tests
+ *
+ * @author     Tom Valk <tomvalk@lt-box.info>
+ * @copyright  2016 Tom Valk
+ */
+
+namespace Arvici\Tests\Heart\Router;
+
+/**
+ * Router Tests
+ * @package Arvici\Tests\Heart\Router
+ *
+ * @coversDefaultClass \Arvici\Heart\Router\Router
+ */
+class RouterTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testDefine()
+    {
+        $this->assertTrue(true);
+    }
+}
