f7a1d91031e768bec3a90df5fc5232c78c43eff3
Tom Valk <tomvalk@gmail.com>
2016-01-24 12:08:50 +0100

    Adding mustache rendering engine to composer and adding several exception classes.

---
+++ src/Arvici/Exception/ConfigurationException.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * ConfigurationException
+ *
+ * @author     Tom Valk <tomvalk@lt-box.info>
+ * @copyright  2016 Tom Valk
+ */
+
+namespace Arvici\Exception;
+
+
+class ConfigurationException extends ArviciException
+{
+    /**
+     * Configuration constructor.
+     * @param string $message
+     * @param int $code
+     * @param \Exception $previous
+     */
+    public function __construct($message, $code = 0, \Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+
+    public function __toString()
+    {
+        return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+    }
+}
