5c7ee9df01f9b49438d74ae623f4d08cbf6b7e46
Tom Valk <tomvalk@gmail.com>
2016-01-25 20:41:57 +0100

    Prepare structure for adding logger.

---
+++ src/Arvici/Exception/LogException.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * LogException
+ *
+ * @author     Tom Valk <tomvalk@lt-box.info>
+ * @copyright  2016 Tom Valk
+ */
+
+namespace Arvici\Exception;
+
+
+class LogException extends ArviciException
+{
+    /**
+     * Configuration constructor.
+     * @param string $message
+     * @param int $code
+     * @param \Exception $previous
+     */
+    public function __construct($message, $code = 0, \Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+
+    public function __toString()
+    {
+        return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+    }
+}
---
+++ src/Arvici/Heart/Log/ExceptionHandler.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * ExceptionHandler
+ *
+ * @author     Tom Valk <tomvalk@lt-box.info>
+ * @copyright  2016 Tom Valk
+ */
+
+namespace logs;
+
+/**
+ * Exception Handler for logging.
+ *
+ * @package logs
+ */
+class ExceptionHandler
+{
+
+}
