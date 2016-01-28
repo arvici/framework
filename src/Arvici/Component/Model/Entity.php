4be6b9b88dbf2dae7fbb020cb0f070bf32dd4d75
Tom Valk <tomvalk@gmail.com>
2016-01-28 21:57:17 +0100

    Adding SweetORM Dependency and structure. Prepare for further integration.

---
+++ src/Arvici/Component/Model/Entity.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Entity Base class (will only referer to SweetORM)
+ *
+ * @author     Tom Valk <tomvalk@lt-box.info>
+ * @copyright  2016 Tom Valk
+ */
+
+namespace Arvici\Component\Model;
+
+use \SweetORM\Entity as SweetEntity;
+
+/**
+ * SweetORM Entity Class
+ *
+ * @package Arvici\Component\Model
+ */
+class Entity extends SweetEntity
+{
+}
