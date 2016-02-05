da1aa3559fe7cf34424194db2caa1ce43e5ed4c7
Tom Valk <tomvalk@gmail.com>
2016-02-05 16:26:55 +0100

    Adding exceptions to prepare input validation.

---
+++ src/Arvici/Exception/InputException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * InputException
+ *
+ * @author     Tom Valk <tomvalk@lt-box.info>
+ * @copyright  2016 Tom Valk
+ */
+
+namespace Arvici\Exception;
+
+
+abstract class InputException extends ArviciException
+{
+}
---
+++ src/Arvici/Exception/ValidationException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * ValidationException
+ *
+ * @author     Tom Valk <tomvalk@lt-box.info>
+ * @copyright  2016 Tom Valk
+ */
+
+namespace Arvici\Exception;
+
+
+class ValidationException extends ArviciException
+{
+}
