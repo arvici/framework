1d6c43cc3f6e1f11297224c38c2424f90f152723
Tom Valk <tomvalk@gmail.com>
2016-02-05 16:07:11 +0100

    Adding Input and Output config files.

---
+++ tests/app/App/Config/Input.php
@@ -0,0 +1,20 @@
+<?php
+use Arvici\Heart\Config\Configuration as Configure;
+
+
+/**
+ * Config: input
+ */
+Configure::define('input', function () {
+    return [
+
+        /**
+         * Input - Validation
+         * Validation sets.
+         */
+        'validationSets' => [
+
+        ],
+
+    ];
+});
---
+++ tests/app/App/Config/Output.php
@@ -0,0 +1,11 @@
+<?php
+use Arvici\Heart\Config\Configuration as Configure;
+
+
+/**
+ * Config: output
+ */
+Configure::define('output', function () {
+    return [
+    ];
+});
