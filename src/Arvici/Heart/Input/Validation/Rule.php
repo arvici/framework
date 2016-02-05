9a6d942b695a1bbccda64bbc5b97afd9c8d3c3b5
Tom Valk <tomvalk@gmail.com>
2016-02-05 16:28:41 +0100

    Adding base rule definition class.

---
+++ src/Arvici/Heart/Input/Validation/Rule.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Rule
+ *
+ * @author     Tom Valk <tomvalk@lt-box.info>
+ * @copyright  2016 Tom Valk
+ */
+
+namespace Arvici\Heart\Input\Validation;
+
+/**
+ * Validation Rule Data Class
+ * @package Arvici\Heart\Input\Validation
+ */
+class Rule
+{
+
+}
