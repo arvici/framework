c40acf99359624832210f7a3141de18881d937cb
Tom Valk <tomvalk@gmail.com>
2017-07-04 13:19:01 +0200

    [TASK] Add more orm commands.

--- src/Arvici/Heart/Console/Application.php
+++ src/Arvici/Heart/Console/Application.php
@@ -100,8 +100,10 @@ class Application extends BaseApplication
             new \Doctrine\ORM\Tools\Console\Command\GenerateRepositoriesCommand(),
             new \Doctrine\ORM\Tools\Console\Command\InfoCommand(),
             new \Doctrine\ORM\Tools\Console\Command\MappingDescribeCommand(),
+            new \Doctrine\ORM\Tools\Console\Command\ConvertMappingCommand(),
             new \Doctrine\ORM\Tools\Console\Command\ValidateSchemaCommand(),
             new \Doctrine\ORM\Tools\Console\Command\RunDqlCommand(),
+            new \Doctrine\ORM\Tools\Console\Command\EnsureProductionSettingsCommand(),
 
             new \Doctrine\ORM\Tools\Console\Command\SchemaTool\CreateCommand(),
             new \Doctrine\ORM\Tools\Console\Command\SchemaTool\DropCommand(),
